2fe62a5c8ab72d042e6b974ee298ec1381dd6e6d
kaushlendrasingh132 <159145949+kaushlendrasingh132@users.noreply.github.com>
2025-07-21 11:43:07 +0530

    Update main.tf

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -3,5 +3,5 @@ resource "azurerm_subnet" "subnet" {
   name                 =each.value.name
   resource_group_name  = each.value.resource_group_name
   virtual_network_name = each.value.virtual_network_name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = ["10.0.1.0/16"]
 }
